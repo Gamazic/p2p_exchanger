2220ab68de63f5b325a22f01d5c48084b4c643d9
Gamazic <nikita.nov.ru@gmail.com>
2022-10-22 23:33:04 +0300

    add p2p order domain entity

---
+++ src/domain/p2p.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from pydantic import BaseModel, NonNegativeFloat, root_validator
+
+from src.repository.binance_api.models import (CryptoCurrency, FiatCurrency,
+                                               P2PTradeType, Payments)
+
+AnyCurrency = FiatCurrency | CryptoCurrency
+
+
+class P2PSameCurrencyError(Exception):
+    ...
+
+
+class P2PTradeTypeError(Exception):
+    ...
+
+
+class P2POrder(BaseModel):
+    source_currency: AnyCurrency
+    target_currency: AnyCurrency
+    price: NonNegativeFloat
+    amount: NonNegativeFloat
+    trade_type: P2PTradeType
+    payments: list[Payments]
+    datetime: datetime
+
+    @root_validator
+    def check_different_currencies_type(cls, values):
+        source_currency = values["source_currency"]
+        target_currency = values["target_currency"]
+        if type(source_currency) == type(target_currency):
+            raise P2PSameCurrencyError(
+                "source currency and target currency should be different type"
+            )
+        return values
+
+    @root_validator
+    def check_trade_type_corresponds_to_source_currency(cls, values):
+        source_currency = values["source_currency"]
+        trade_type = values["trade_type"]
+        if source_currency in FiatCurrency and trade_type is P2PTradeType.SELL:
+            raise P2PTradeTypeError(
+                "source currency couldn't be Fiat with SELL trade type"
+            )
+        elif source_currency in CryptoCurrency and trade_type is P2PTradeType.BUY:
+            raise P2PTradeTypeError(
+                "source currency couldn't be Crypto with BUY trade type"
+            )
+        return values
+
+
+class P2PFilter(BaseModel):
+    source_currency: AnyCurrency
+    target_currency: AnyCurrency
+    min_amount: NonNegativeFloat
+    payments: list[Payments]
---
+++ tests/domain/test_p2p.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+import pytest
+
+from src.domain.p2p import P2POrder, P2PSameCurrencyError, P2PTradeTypeError
+from src.repository.binance_api.models import (CryptoCurrency, FiatCurrency,
+                                               P2PTradeType)
+
+
+class TestP2POrder:
+    def test_same_currency(self):
+        with pytest.raises(P2PSameCurrencyError):
+            P2POrder(
+                source_currency=FiatCurrency.RUB,
+                target_currency=FiatCurrency.KZT,
+                price=0,
+                amount=0,
+                trade_type=P2PTradeType.BUY,
+                payments=[],
+                datetime=datetime.now(),
+            )
+        with pytest.raises(P2PSameCurrencyError):
+            P2POrder(
+                source_currency=CryptoCurrency.USDT,
+                target_currency=CryptoCurrency.BTC,
+                price=0,
+                amount=0,
+                trade_type=P2PTradeType.BUY,
+                payments=[],
+                datetime=datetime.now(),
+            )
+
+    def test_bad_trade_type(self):
+        with pytest.raises(P2PTradeTypeError):
+            P2POrder(
+                source_currency=FiatCurrency.RUB,
+                target_currency=CryptoCurrency.USDT,
+                price=0,
+                amount=0,
+                trade_type=P2PTradeType.SELL,
+                payments=[],
+                datetime=datetime.now(),
+            )
+        with pytest.raises(P2PTradeTypeError):
+            P2POrder(
+                source_currency=CryptoCurrency.USDT,
+                target_currency=FiatCurrency.RUB,
+                price=0,
+                amount=0,
+                trade_type=P2PTradeType.BUY,
+                payments=[],
+                datetime=datetime.now(),
+            )
