15e15d1b9c41a45c6238e7dcec47ed49eecad545
Gamazic <nikita.nov.ru@gmail.com>
2022-10-24 17:33:46 +0300

    add testcase for not registered payments

--- tests/repository/test_models.py
+++ tests/repository/test_models.py
@@ -29,11 +29,12 @@ EXAMPLE_SEARCH_API_ARG = SearchApiParams(
 )
 
 
-def test_params():
-    assert (
-        EXAMPLE_SEARCH_API_ARG.dict(by_alias=True, exclude_none=True)
-        == EXAMPLE_RAW_SEARCH_API_PARAMS
-    )
+class TestSearchApiParams:
+    def test_params(self):
+        assert (
+            EXAMPLE_SEARCH_API_ARG.dict(by_alias=True, exclude_none=True)
+            == EXAMPLE_RAW_SEARCH_API_PARAMS
+        )
 
 
 EXAMPLE_RAW_SEARCH_API_RESPONSE = {
@@ -142,6 +143,112 @@ EXAMPLE_RAW_SEARCH_API_RESPONSE = {
     "total": 504,
     "success": True,
 }
+EXAMPLE_RAW_SEARCH_API_RESPONSE_UNSEEN_PAYMENT = {
+    "code": "000000",
+    "message": None,
+    "messageDetail": None,
+    "data": [
+        {
+            "adv": {
+                "advNo": "11416653472193486848",
+                "classify": "mass",
+                "tradeType": "SELL",
+                "asset": "USDT",
+                "fiatUnit": "RUB",
+                "advStatus": None,
+                "priceType": None,
+                "priceFloatingRatio": None,
+                "rateFloatingRatio": None,
+                "currencyRate": None,
+                "price": "61.82",
+                "initAmount": None,
+                "surplusAmount": "395.97",
+                "amountAfterEditing": None,
+                "maxSingleTransAmount": "5001.00",
+                "minSingleTransAmount": "5000.00",
+                "buyerKycLimit": None,
+                "buyerRegDaysLimit": None,
+                "buyerBtcPositionLimit": None,
+                "remarks": None,
+                "autoReplyMsg": "",
+                "payTimeLimit": None,
+                "tradeMethods": [
+                    {
+                        "payId": None,
+                        "payMethodId": "",
+                        "payType": None,
+                        "payAccount": None,
+                        "payBank": None,
+                        "paySubBank": None,
+                        "identifier": "IDK",
+                        "iconUrlColor": None,
+                        "tradeMethodName": "Тинькофф",
+                        "tradeMethodShortName": "Тинькофф",
+                        "tradeMethodBgColor": "#DAB700",
+                    }
+                ],
+                "userTradeCountFilterTime": None,
+                "userBuyTradeCountMin": None,
+                "userBuyTradeCountMax": None,
+                "userSellTradeCountMin": None,
+                "userSellTradeCountMax": None,
+                "userAllTradeCountMin": None,
+                "userAllTradeCountMax": None,
+                "userTradeCompleteRateFilterTime": None,
+                "userTradeCompleteCountMin": None,
+                "userTradeCompleteRateMin": None,
+                "userTradeVolumeFilterTime": None,
+                "userTradeType": None,
+                "userTradeVolumeMin": None,
+                "userTradeVolumeMax": None,
+                "userTradeVolumeAsset": None,
+                "createTime": None,
+                "advUpdateTime": None,
+                "fiatVo": None,
+                "assetVo": None,
+                "advVisibleRet": None,
+                "assetLogo": None,
+                "assetScale": 2,
+                "fiatScale": 2,
+                "priceScale": 2,
+                "fiatSymbol": "₽",
+                "isTradable": True,
+                "dynamicMaxSingleTransAmount": "5001.00",
+                "minSingleTransQuantity": "80.87",
+                "maxSingleTransQuantity": "80.89",
+                "dynamicMaxSingleTransQuantity": "80.89",
+                "tradableQuantity": "395.57",
+                "commissionRate": "0.00100000",
+                "tradeMethodCommissionRates": [],
+                "launchCountry": None,
+                "abnormalStatusList": None,
+                "closeReason": None,
+            },
+            "advertiser": {
+                "userNo": "s987ef83f7c373b2d939960f36abde794",
+                "realName": None,
+                "nickName": "Kripto-Monah",
+                "margin": None,
+                "marginUnit": None,
+                "orderCount": None,
+                "monthOrderCount": 210,
+                "monthFinishRate": 0.996,
+                "advConfirmTime": None,
+                "email": None,
+                "registrationTime": None,
+                "mobile": None,
+                "userType": "user",
+                "tagIconUrls": [],
+                "userGrade": 2,
+                "userIdentity": "",
+                "proMerchant": None,
+                "isBlocked": None,
+            },
+        }
+    ],
+    "total": 504,
+    "success": True,
+}
 EXAMPLE_SEARCH_API_RETURN = SearchApiResponse(
     code="000000",
     data=[
@@ -167,6 +274,12 @@ EXAMPLE_SEARCH_API_RETURN = SearchApiResponse(
 )
 
 
-def test_response():
-    parsed_search_api = SearchApiResponse.parse_obj(EXAMPLE_RAW_SEARCH_API_RESPONSE)
-    assert EXAMPLE_SEARCH_API_RETURN == parsed_search_api
+class TestSearchApiResponse:
+    def test_response(self):
+        parsed_search_api = SearchApiResponse.parse_obj(EXAMPLE_RAW_SEARCH_API_RESPONSE)
+        assert EXAMPLE_SEARCH_API_RETURN == parsed_search_api
+
+    def test_unseen_payment(self):
+        parsed_search_api = SearchApiResponse.parse_obj(
+            EXAMPLE_RAW_SEARCH_API_RESPONSE_UNSEEN_PAYMENT
+        )
