7a2278e808700d5f060059649482b2e6f95efbf1
Gamazic <nikita.nov.ru@gmail.com>
2022-10-26 14:40:31 +0300

    remove gen certs and build scripts

--- .do/build_command.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-CERTS_DIR="./$(dirname $0)/../certs"
-
-cd $CERTS_DIR
-./gen-certs.sh
--- certs/gen-certs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-HOST="$1"
-
-openssl genrsa -out cert.pem 2048 &&\
-  openssl req -new -x509 -days 365 -key cert.pem -out pubkey.pem -subj "/CN=${HOST}"
