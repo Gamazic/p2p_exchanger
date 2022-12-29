f80c3b35fb9f8e4556265d53f0db1c98087914ee
Gamazic <nikita.nov.ru@gmail.com>
2022-12-29 15:02:56 +0300

    currently remove try and gel currencies

--- src/interfaces/aiogram/models.py
+++ src/interfaces/aiogram/models.py
@@ -2,15 +2,11 @@ from enum import Enum
 
 RUB_PAYMENTS = ["TinkoffNew", "QIWI", "Payeer"]
 KZT_PAYMENTS = ["KaspiBank", "JysanBank"]
-TRY_PAYMENTS = ["Ziraat", "QNB"]
-GEL_PAYMENTS = ["BankofGeorgia", "LIBERTYBANK", "TBCbank"]
 
 
 class FiatCurrency(Enum):
     RUB = "RUB"
     KZT = "KZT"
-    TRY = "TRY"
-    GEL = "GEL"
 
 
 class CryptoCurrency(Enum):
@@ -22,6 +18,4 @@ class CryptoCurrency(Enum):
 PAYMENTS_CASE = {
     FiatCurrency.RUB.value: RUB_PAYMENTS,
     FiatCurrency.KZT.value: KZT_PAYMENTS,
-    FiatCurrency.TRY.value: TRY_PAYMENTS,
-    FiatCurrency.GEL.value: GEL_PAYMENTS,
 }
